57e64a6290d0459f03e2a22c317230fe46d02d19
Suga Takuya <takuyasuga0109@gmail.com>
2020-12-14 01:08:07 +0900

    Update

--- Bokeh/main.py
+++ Bokeh/main.py
@@ -2,7 +2,7 @@ from bokeh.models import DataTable, TableColumn, PointDrawTool, ColumnDataSource
 from bokeh.plotting import figure, output_file, show, Column, Row
 from bokeh.io import curdoc, save
 
-from bokeh.models.widgets import Button
+from bokeh.models.widgets import Button, Slider
 from bokeh.themes import built_in_themes
 import pandas as pd
 import requests
@@ -16,12 +16,58 @@ from PillowRoundedRecCreation import word_image_creation
 
 def btn_wcrd_onclick():
     df_wcrd = df_crd[df_crd["color"] != "Thumbnail"]
+    p_c_x_list_wcrd = df_wcrd["p_c_x"].values.tolist()
+    p_c_y_list_wcrd = df_wcrd["p_c_y"].values.tolist()
+
+    word_list_wcrd = df_wcrd["word"].values.tolist()
+    url_list_wcrd = list(map(lambda x: "Bokeh/static/IMAGEs/" + x + ".png", word_list_wcrd))
+
+    p_br_x_list_wcrd = df_wcrd["p_br_x"].values.tolist()
+    p_bl_x_list_wcrd = df_wcrd["p_bl_x"].values.tolist()
+    w_list_wcrd = (np.array(p_br_x_list_wcrd) - np.array(p_bl_x_list_wcrd)).tolist()
+
+    p_bl_y_list_wcrd = df_wcrd["p_bl_y"].values.tolist()
+    p_tl_y_list_wcrd = df_wcrd["p_tl_y"].values.tolist()
+    h_list_wcrd = (np.array(p_bl_y_list_wcrd) - np.array(p_tl_y_list_wcrd)).tolist()
+    source_crd.data = {'p_c_x'   : p_c_x_list_wcrd, 'p_c_y': p_c_y_list_wcrd,\
+                       'url_list': url_list_wcrd,   'w'    : w_list_wcrd,   'h' : h_list_wcrd}
+
+def btn_tcrd_onclick():
+    df_tcrd  = df_crd[df_crd["color"] == "Thumbnail"]
+    p_c_x_list = df_tcrd["p_c_x"].values.tolist()
+    p_c_y_list = df_tcrd["p_c_y"].values.tolist()
+
+    word_list = df_tcrd["word"].values.tolist()
+    url_list = list(map(lambda x: "Bokeh/static/IMAGEs/" + x + ".png", word_list))
+
+    p_br_x_list = df_tcrd["p_br_x"].values.tolist()
+    p_bl_x_list = df_tcrd["p_bl_x"].values.tolist()
+    w_list = (np.array(p_br_x_list) - np.array(p_bl_x_list)).tolist()
+
+    p_bl_y_list = df_tcrd["p_bl_y"].values.tolist()
+    p_tl_y_list = df_tcrd["p_tl_y"].values.tolist()
+    h_list = (np.array(p_bl_y_list) - np.array(p_tl_y_list)).tolist()
     
-    source_crd = ColumnDataSource(
-    data = {'p_c_x': p_c_x_list, 'p_c_y': p_c_y_list,\
+    source_crd.data = {'p_c_x': p_c_x_list, 'p_c_y': p_c_y_list,\
             'url_list': url_list, 'w' : w_list, 'h' : h_list}
-    )
+
+def btn_wt_crd_onclick():
+    p_c_x_list = df_crd["p_c_x"].values.tolist()
+    p_c_y_list = df_crd["p_c_y"].values.tolist()
+
+    word_list = df_crd["word"].values.tolist()
+    url_list = list(map(lambda x: "Bokeh/static/IMAGEs/" + x + ".png", word_list))
+
+    p_br_x_list = df_crd["p_br_x"].values.tolist()
+    p_bl_x_list = df_crd["p_bl_x"].values.tolist()
+    w_list = (np.array(p_br_x_list) - np.array(p_bl_x_list)).tolist()
+
+    p_bl_y_list = df_crd["p_bl_y"].values.tolist()
+    p_tl_y_list = df_crd["p_tl_y"].values.tolist()
+    h_list = (np.array(p_bl_y_list) - np.array(p_tl_y_list)).tolist()
     
+    source_crd.data = {'p_c_x': p_c_x_list, 'p_c_y': p_c_y_list,\
+            'url_list': url_list, 'w' : w_list, 'h' : h_list}
 
 
 #df_sx   = pd.read_csv("../CSVs/S_X_output.csv")
@@ -73,8 +119,7 @@ w_list = (np.array(p_br_x_list) - np.array(p_bl_x_list)).tolist()
 p_bl_y_list = df_crd["p_bl_y"].values.tolist()
 p_tl_y_list = df_crd["p_tl_y"].values.tolist()
 h_list = (np.array(p_bl_y_list) - np.array(p_tl_y_list)).tolist()
-
-
+###***************************************************************************
 for index, row in df_crd.iterrows():
     if not os.path.exists('Bokeh/static/IMAGEs/' + row['word']  + ".png"):
         if row['color'] != 'Thumbnail':
@@ -89,13 +134,15 @@ for index, row in df_crd.iterrows():
             img = Image.open("Bokeh/static/IMAGEs/" + row['word']  + ".png")
             img_resize = img.resize( (int((row['p_br_x'] - row['p_bl_x']) / 2), int((row['p_bl_y'] - row['p_tl_y']) / 2)) )
 
+
 source_crd = ColumnDataSource(
     data = {'p_c_x': p_c_x_list, 'p_c_y': p_c_y_list,\
             'url_list': url_list, 'w' : w_list, 'h' : h_list}
 )
 
+
 p_crd =  figure(plot_width=900, plot_height=540,
-               title="Crowd", match_aspect=True) 
+               title="Crowd", match_aspect=True, tools='pan,wheel_zoom,box_zoom,undo,redo,save,reset,help') 
 
 r1 = p_crd.circle(x='p_c_x', y='p_c_y', source=source_crd, color='red', alpha=1, size=10)
 image1 = ImageURL(url="url_list", x="p_c_x", y="p_c_y", w="w", h="h", anchor="center")
@@ -106,16 +153,27 @@ p_crd.add_tools(draw_tool)
 p_crd.toolbar.active_tap = draw_tool
 
 
-btn_wcrd = Button(label="Word",  button_type="success")
+btn_wcrd = Button(label="Word")
 btn_wcrd.on_click(btn_wcrd_onclick)
-btn_twrd = Button(label="Thumbnail", button_type="success")
-btn_twrd.on_click(btn_twrd_onclick)
+btn_tcrd = Button(label="Thumbnail", button_type="success")
+btn_tcrd.on_click(btn_tcrd_onclick)
 btn_wt_crd = Button(label="Word & Thumbnail",  button_type="success")
 btn_wt_crd.on_click(btn_wt_crd_onclick)
 
-buttons = Row(btn_crd, btn_thmb, btn_crd_and_thmb)
+thubmnail_alpha_slider = Slider(start=0, end=10, value=1, step=.1, title="Stuff")
+slider = Slider(start=0, end=1, value=1, step=.1, title="Stuff")
+js_on_change('value', callback)
 
-p_crd_layout = Column(p_crd, buttons)
+buttons = Row(btn_wcrd, btn_tcrd, btn_wt_crd)
+
+columns = [
+        TableColumn(field="p_c_x", title="p_c_x"),
+        TableColumn(field="p_c_y", title="p_c_y")
+          ]
+data_table = DataTable(source=source_crd, columns=columns, width=400, height=280)
+
+
+p_crd_layout = Column(p_crd, buttons, data_table)
 
 #最後の部分
 plots = Row(p_sx, p_crd_layout)
@@ -125,39 +183,4 @@ curdoc().add_root(plots)
 
 
 
-
-'''
-plot = figure(plot_width=400, plot_height=400) #p = <class 'bokeh.plotting.figure.Figure'>
-
-source = ColumnDataSource(
-    data = {'x': [1, 5, 9], 'y': [1, 5, 9], 'url' : ['1.png', '2.png', '1.png']}
-)
-
-#renderer = p.scatter(x='x', y='y', source=source, color='color', size=10)
-#r1 = plot.circle(x='x', y='y', source=source, color='red', size=10)
-#r1 = <class 'bokeh.models.renderers.GlyphRenderer'>
-r2 = plot.image_url(url='url' ,x='x', y='y', w=0.8, h=0.6,source=source, anchor="center")
-#image1 = ImageURL(url=['1.png'], x="x", y="y", w=0.6, h=0.6, anchor="center", global_alpha = 0.5)
-#r2 = plot.add_glyph(source, image1)
-print(type(r2))
-#r2 = <class 'bokeh.models.renderers.GlyphRenderer'>
-
-#r2 = p.image_url(url=['2.png'], x=1, y=3, w=0.8, h=0.6)
-
-draw_tool = PointDrawTool(renderers=[r2])
-plot.add_tools(draw_tool)
-plot.toolbar.active_tap = draw_tool
-
-columns = [TableColumn(field="x", title="x"),
-           TableColumn(field="y", title="y"),
-           TableColumn(field='color', title='url')]
-table = DataTable(source=source, columns=columns, editable=True, height=200)
-
-button_area = Column(sepal_button, petal_button, table)
-layout = Row(button_area, plot)
-output_file("MOGE.html")
-show(layout)
-#curdoc().add_root(layout)
-
-'''
 
